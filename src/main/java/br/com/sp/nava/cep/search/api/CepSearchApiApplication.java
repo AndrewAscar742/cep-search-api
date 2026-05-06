2bc3232d95fc4203144d75e0e58b31b67afc2d6e
Andrew Matos Ascar <andrewmatosascar@outlook.com>
2026-05-05 23:19:16 -0300

    Initial Commit

---
+++ src/main/java/br/com/sp/nava/cep/search/api/CepSearchApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.sp.nava.cep.search.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CepSearchApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CepSearchApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/sp/nava/cep/search/api/CepSearchApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sp.nava.cep.search.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CepSearchApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
