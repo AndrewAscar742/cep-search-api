f7828f69f71138172dc4f6b36262fcb37adeecb1
Andrew Matos Ascar <andrewmatosascar@outlook.com>
2026-05-06 23:41:30 -0300

    feat: implementação da configuração do restClientConfig

---
+++ src/main/java/br/com/sp/nava/cep/search/api/config/RestClientConfig.java
@@ -0,0 +1,41 @@
+package br.com.sp.nava.cep.search.api.config;
+
+import java.time.Duration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.JdkClientHttpRequestFactory;
+import org.springframework.web.client.RestClient;
+
+@Configuration
+@Profile(value = "prd")
+public class RestClientConfig {
+	
+    @Value("${viacep.base-url}")
+    private String baseUrl;
+	
+    @Value("${viacep.connect-timeout-ms}")
+    private int connectTimeoutMs;
+
+    @Value("${viacep.read-timeout-ms}")
+    private int readTimeoutMs;
+	
+    @Bean
+    public RestClient gatewayRestClient() {
+        JdkClientHttpRequestFactory requestFactory = new JdkClientHttpRequestFactory();
+        requestFactory.setReadTimeout(Duration.ofMillis(readTimeoutMs));
+
+        return RestClient.builder()
+                .baseUrl(baseUrl)
+                .requestFactory(requestFactory)
+                .defaultHeaders(headers -> {
+                    headers.set(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE);
+                    headers.set(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
+                })
+                .build();
+    }
+}
