09c1d13bead439ba020c76431424101117c15ae3
Andrew Matos Ascar <andrewmatosascar@outlook.com>
2026-05-10 17:04:01 -0300

    feat: adição da camada repository

---
+++ src/main/java/br/com/sp/nava/cep/search/api/repository/EnderecoRepository.java
@@ -0,0 +1,9 @@
+package br.com.sp.nava.cep.search.api.repository;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EnderecoRepository extends JpaRepository<EnderecoRepository, UUID>{
+
+}
